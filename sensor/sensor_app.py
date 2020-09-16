c91f9864a0d059e08509ce83b19dc95cec3bc7ed
Kedar D <kedarshri@gmail.com>
2020-09-15 21:59:20 -0700

    first module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,6 @@
 # Runner script for all modules
 
-
+from load_data import load_sensor_data
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -9,7 +9,9 @@ print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
+data=load_sensor_data()
 
+print("Loaded records:  {}".format(len(data)))
 # Module 2 code here:
 
 # Module 3 code here:
